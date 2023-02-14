f08079e1cc6b5773261ac611bbf1cf56a153c08d
Keero <36694122+keerobommera@users.noreply.github.com>
2023-02-14 22:11:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="./css/style.css?<?php echo time();?>">
+</html>
+<body>
+    <div class="launch">
+
+    
+    <h1> Launching Soon</h1>
+    </div>
+</body>
